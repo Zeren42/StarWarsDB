475dbf0eec66f8fc4a5e71402785060e1df5437b
Breno Santos <39634059+Zeren42@users.noreply.github.com>
2024-09-07 20:24:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+function pesquisar() {
+    // Função responsável por pesquisar e exibir os resultados na página.
+    // Itera sobre os dados e cria elementos HTML para cada item.
+  
+    // Seleciona a seção onde os resultados serão exibidos
+    const section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    
+    if (campoPesquisa == ""){
+      section.innerHTML = "<p>Digite algo para buscar.</p>"
+      return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+    console.log(campoPesquisa)
+  
+    // Inicializa uma string vazia para armazenar os resultados HTML
+    // Essa string será preenchida com os elementos HTML gerados no loop.
+    let resultados = "";
+    let nome = "";
+    let descricao = "";
+    let tags = "";
+    // Itera sobre cada elemento do array de dados
+    // Para cada elemento (dado), cria um novo elemento HTML e o adiciona à string 'resultados'.
+    for (let dado of dados) {
+      nome = dado.nome.toLowerCase();
+      descricao = dado.descricao.toLowerCase();
+      tags = dado.tags.toLowerCase();
+        if(nome.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+      // Constrói o HTML para cada item, incluindo imagem, título, descrição e link
+      resultados += `
+        <div class='item-resultado'>
+          <img class="foto" src=${dado.foto}>
+          <h2>
+            <a href="#" target="_blank" rel="noopener noreferrer">${dado.nome}</a>
+          </h2>
+          <p class='descricao-meta'>
+            ${dado.descricao}
+          </p>
+          <a href=${dado.link} target='_blank'>Saiba Mais</a>
+        </div>
+      `;
+        }
+  
+    }
+    
+    if (!resultados){
+      resultados = "<p>Nenhum resultado encontrado.</p>"
+
+    }
+  
+    // Atribui o HTML gerado para o conteúdo da seção
+    // Substitui todo o conteúdo da seção pelo HTML gerado.
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,55 @@
+let dados = [ 
+    {
+        nome: "Darth Vader",
+        descricao: "Darth Vader é um dos principais antagonistas da saga Star Wars. Um poderoso Lord Sith, ele é conhecido por sua força na Força e seu imponente traje negro. Antes de se tornar um Sith, era o jovem Anakin Skywalker, um Jedi que se voltou para o lado sombrio da Força.",
+        tipo: "personagem",
+        link: "https://starwars.fandom.com/pt/wiki/Anakin_Skywalker",
+        foto: "https://rollingstone.com.br/media/_versions/darth_vader_div_lucasfilm_widelg.jpg",
+        tags: ""
+
+    },
+    {
+        nome: "Leia Organa",
+        descricao: "Princesa de Alderaan e líder da Aliança Rebelde, Leia é uma habilidosa estrategista e uma figura fundamental na luta contra o Império Galáctico.",
+        tipo: "personagem",
+        link: "https://starwars.fandom.com/pt/wiki/Leia_Organa",
+        foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Princess_Leia_Organa.jpg/220px-Princess_Leia_Organa.jpg",
+        tags: ""
+        
+    },
+    {
+        nome: "Yoda",
+        descricao: "Mestre Jedi sábio e poderoso, Yoda é um dos personagens mais antigos da galáxia e um dos maiores mestres da Força.",
+        tipo: "personagem",
+        link: "https://starwars.fandom.com/pt/wiki/Yoda",
+        foto: "https://static.wikia.nocookie.net/starwars/images/1/1d/Yoda_TPM.jpg/revision/latest?cb=20170715192127",
+        tags: ""
+    },
+    {
+        nome: "Han Solo",
+        descricao: "Contrabandista sarcástico e habilidoso piloto da Millennium Falcon, Han Solo é um companheiro leal e um dos heróis da Aliança Rebelde.",
+        tipo: "personagem",
+        link: "[https://starwars.fandom.com/pt/wiki/Han_Solo](https://starwars.fandom.com/pt/wiki/Han_Solo)",
+        foto: "#",
+        tags: ""
+    },
+    {
+        nome: "Sabre de Luz",
+        descricao: "Arma característica dos Jedi e Sith, o sabre de luz é uma arma de energia poderosa, cuja lâmina é criada por um cristal kyber.",
+        tipo: "item",
+        link: "https://starwars.fandom.com/pt/wiki/Sabre_de_luz",
+        foto: "https://static.wikia.nocookie.net/starwars/images/a/a5/Lightsaber.jpg/revision/latest?cb=20170715192543",
+        tags: ""
+    },
+    {
+        nome: "Millennium Falcon",
+        descricao: "Nave espacial contrabandeada, famosa por sua velocidade e manobrabilidade, a Millennium Falcon é pilotada por Han Solo e Chewbacca.",
+        tipo: "item",
+        link: "https://starwars.fandom.com/pt/wiki/Millennium_Falcon",
+        foto: "https://static.wikia.nocookie.net/ptstarwars/images/d/dc/Melfal.jpg/",
+        tags: ""
+    }
+]
+dados.forEach(dado => {
+    
+})
